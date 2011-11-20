aec0100d342a5321e77f7b799ec608067d2dd09a
juergen.lipp@gmail.com <juergen.lipp@gmail.com@5a0abe33-c8ad-b0c2-79bb-06902ab4bf64>
2011-11-20 12:23:18 +0000

    Display error stack instead of exception string, for that purpose DBMS_UTILITY is internally called, error message string replaced by bool value to indicate whether to file stack or not

--- trunk/src/Core/LoggingEventBody.sql
+++ trunk/src/Core/LoggingEventBody.sql
@@ -1,31 +1,35 @@
---
--- Copyright 2011 Jürgen Lipp
--- 
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
--- 
---     http://www.apache.org/licenses/LICENSE-2.0
--- 
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
---
+/** 
+* Copyright 2011 Juergen Lipp
+*	
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 create or replace 
 type body LoggingEvent as
-
-  constructor function LoggingEvent(loggerName varchar2, logLevel LogLevel, message varchar2, exceptionString varchar2) return self as result as
-  begin
-    self.LoggerName := loggerName;
-    self.LLevel := logLevel;
-    self.DateTime := SYSTIMESTAMP;
-    self.UserName := user;
-    self.Message := message;
-    self.ExceptionString := exceptionString;
-    return;
-  end;
-  
+	
+	constructor function LoggingEvent(loggerName varchar2, logLevel LogLevel, message varchar2, showError boolean) return self as result as
+	begin
+		self.LoggerName := loggerName;
+		self.LLevel := logLevel;
+		self.DateTime := SYSTIMESTAMP;
+		self.UserName := user;
+		self.Message := message;
+		if showError then
+			self.ExceptionString := dbms_utility.format_error_stack||dbms_utility.format_error_backtrace;
+		end if;
+		
+		return;
+	end;
+	
 end;
 /

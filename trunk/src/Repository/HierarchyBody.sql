9f3abdcdbc00318f0ef24109d7aa9b0352b0d5c8
DavidPyke@gmail.com <DavidPyke@gmail.com@5a0abe33-c8ad-b0c2-79bb-06902ab4bf64>
2013-02-10 22:40:49 +0000

    issue #1 - move root initialisation to package init

--- trunk/src/Repository/HierarchyBody.sql
+++ trunk/src/Repository/HierarchyBody.sql
@@ -41,9 +41,6 @@ package body Hierarchy as
 	
 	function GetLogger(name varchar2) return Logger is
 	begin
-		if Hierarchy.Root is null then
-			Hierarchy.Root := LoggerImpl(0, LogLevel.Debug, 'root', null, AppenderArray());
-		end if;
 		return LoggerImpl(1, null, name, anydata.ConvertObject(Hierarchy.Root), AppenderArray());
 	end;
 	
@@ -67,5 +64,9 @@ package body Hierarchy as
 		raise LogUtil.NotImplementedException; 
 	end;
 
+begin
+		if Hierarchy.Root is null then
+			Hierarchy.Root := LoggerImpl(0, LogLevel.Debug, 'root', null, AppenderArray());
+		end if;
 end Hierarchy;
 /
